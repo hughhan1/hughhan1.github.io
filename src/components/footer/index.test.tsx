966c3d28d2bf4e11602756bf26c3fb76e3be76f5
Hugh Han <hughhan1@gmail.com>
2019-06-16 17:30:01 -0400

    Updates

--- src/components/footer/index.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Footer from './index';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Footer currentPage="" />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
--- src/components/home-page/index.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import HomePage from './index';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<HomePage />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
